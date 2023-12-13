be41c429dc6ed361b98544f92a1bc7c5c208cda3
JooWon <joocode12@gmail.com>
2023-12-13 17:43:59 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	build/classes/java/main/com/kim/dec08rest/controller/AdminUserController.class
    #	build/tmp/compileJava/compileTransaction/stash-dir/AdminUserController.class.uniqueId0
    #	build/tmp/compileJava/previous-compilation-data.bin
    #	src/main/java/com/kim/dec08rest/controller/AdminUserController.java

---
+++ build/resources/main/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values(90001, now(), 'User11', 'tesst11', '111111-1111111');
+insert into users(id, join_date, name, password, ssn) values(90002, now(), 'User12', 'tesst12', '211111-1111111');
+insert into users(id, join_date, name, password, ssn) values(90003, now(), 'User13', 'tesst13', '311111-1111111');
